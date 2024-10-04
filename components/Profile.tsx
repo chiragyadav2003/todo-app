ccabbe21865ca3e3f5a38f54c2a7c2d1647f8cec
chiragyadav2003 <97416399+chiragyadav2003@users.noreply.github.com>
2024-10-04 21:54:01 +0530

    Refactor Profile component to handle user logout and display avatar or initial letter

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -4,10 +4,21 @@ import Link from 'next/link'
 import { Button } from './ui/button'
 import { useUser } from '@/app/hooks/useUser'
 import Image from 'next/image';
+import { useQueryClient } from '@tanstack/react-query';
+import { useRouter } from 'next/navigation';
+import { supabaseBrowser } from '@/utils/supabase/client';
 
 export const Profile = ()=>{
-
   const {isFetching, data} = useUser();
+  const queryClient = useQueryClient();
+  const router = useRouter();
+
+  const handleLogout = async()=>{
+    const supabase = supabaseBrowser();
+    queryClient.clear(); // Clear all cache
+    await supabase.auth.signOut();
+    router.refresh();
+  }
 
   if(isFetching){
     return <></>
@@ -23,15 +34,28 @@ export const Profile = ()=>{
             </Button>
           </Link>
         ) : (
-          <h1>
-            <Image 
-              src={data.image_url || ""} 
-              alt={data.display_name || ""} 
-              width={50}
-              height={50}
-              className='rounded-full animate-fade '
-            />
-          </h1>
+          <>
+            {
+              data?.image_url ? 
+              (
+                <Image 
+                  src={data.image_url || ""} 
+                  alt={data.display_name || ""} 
+                  width={50}
+                  height={50}
+                  className='rounded-full animate-fade cursor-pointer'
+                  onClick={handleLogout}
+                />
+              ) : (
+                <div 
+                  className='h-[50px] w-[50px] flex items-center justify-center ring-2 rounded-full text-2xl font-semibold cursor-pointer' 
+                  onClick={handleLogout}
+                >
+                  <h1>{data.email[0]}</h1>
+                </div>
+              )
+            }
+          </>
         )}
     </div>
   )
